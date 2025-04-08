c9a18c1311fae87b010886b51943c9b50d9649bb
Benjamin Silver <besilver@ucsc.edu>
2025-04-08 14:49:34 -0700

    Adding experiment 1 from glitch

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,84 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+// project.js - A simple fantasy combat generator
+// Author: Ben Silver
+// Date: 4/8/25
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
+const fillers = {
+  weapon: [
+    "Greatsword",
+    "Rapier",
+    "Longsword",
+    "Dagger",
+    "Mace",
+    "Hammer",
+    "Scimitar",
+    "Scythe",
+    "Shardblade",
+  ],
+  action: [
+    "rush",
+    "fall",
+    "crash",
+    "walk",
+  ],
+  enemy: [
+    "Dragon",
+    "Giant",
+    "Thunderclast",
+    "Great Beast",
+  ],
+  attack: [
+    "striking out",
+    "lashing out",
+    "cleaving your foe",
+  ],
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+  defeat: [
+    "collapses under it's own weight and begins to fade to dust",
+    "falls, bleeding out, and looks at you defiantly as the light fades from it's eyes",
+    "smiles, the darkness finally leaving it's eyes before closing for the last time",
+  ],
+  
+  victory: [
+    "feel unsatisfied and begin your long journey out of the beast's lair",
+    "somberly pay your respects to the beast, then smile",
+    "let out a shout filled with vigor",
+    "look around at the corpses of your greatests allies, you fall to your knees, ",
+    "smile as cheers erupt from your companions,",
+  ],
+    
+  
+};
+
+const template = `You $action towards the $enemy $attack with $weapon in hand! You strike true, dealing a lethal blow.
+
+Your foe $defeat. You $victory blood running down your body.
+`;
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
